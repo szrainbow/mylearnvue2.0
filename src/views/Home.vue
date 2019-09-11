50f621e1e89a07edffae10bca9a3d0bda46de294
xuchu <952022363@qq.com>
2019-09-11 16:15:01 +0800

    添加了vue模板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="welcomes"/>
+    <HelloWorld :msg="welcomes"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
     HelloWorld
   },
-  data:{
-    return(){
+  data(){
+    return{
       welcomes:"this content is pull from github git"
     }
   }
